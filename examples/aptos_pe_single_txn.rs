7e0d659499edf72e4b1bc68e96bf3b333ca4e00d
贺梦杰 <njtech_hemengjie@qq.com>
2022-11-18 14:20:52 +0800

    add analyze scripts

--- examples/aptos_pe_single_txn.rs
+++ examples/aptos_pe_single_txn.rs
@@ -4,6 +4,6 @@ fn main() {
         "aptos_pe_single_txn",
         tracing::Level::TRACE,
     );
-    let (txns, state) = block_stm::test_utils::aptos::generate_txns_and_state(5, 10_000);
+    let (txns, state) = block_stm::test_utils::aptos::generate_txns_and_state(1_000, 10_000);
     let _ = block_stm::test_utils::aptos::aptos_parallel_execute(&txns, &state, num_cpus::get());
 }
--- scripts/analyze_single_txn_log.py
+++ scripts/analyze_single_txn_log.py
@@ -2,14 +2,12 @@
 # parse single txn logs
 import re
 import sys
-import csv
+import pandas as pd
 for path in sys.argv[1:]:
     file = open(path, "r")
     content = "".join(file.readlines())
     matches = re.findall(
         "txn_idx=([0-9]+).*reading_walltime=([0-9.]+).*execution_walltime=([0-9.]+).*reading_walltime_percentage=([0-9.]+).*", content)
-    file = open(path+".csv", "w", newline="")
-    writer = csv.writer(file)
-    writer.writerow(["txn_idx", "reading_walltime",
-                    "execution_walltime", "reading_walltime_percentage"])
-    [writer.writerow(match) for match in matches]
+    df = pd.DataFrame(matches, columns=[
+                      "txn_idx", "reading_walltime", "execution_walltime", "reading_walltime_percentage"])
+    df.to_csv(path+".csv", index=False)
